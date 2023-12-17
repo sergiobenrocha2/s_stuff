270d935e2333e922d04fe24c61b8f9c327f462e2
Sergio Rocha <5431285+sergiobenrocha2@users.noreply.github.com>
2023-12-16 22:55:39 -0300

    Created common read input function in setting / reading bits C program

--- C/setting_and_reading_bits.c
+++ C/setting_and_reading_bits.c
@@ -2,12 +2,46 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define MASK 0x01
+#define STR_TEXT_SIZE 50
+
+typedef bool (*valid_range)(long long int, unsigned char);
 
 // change the testing type easily, one spot
 typedef unsigned short wildcard_type;
-#define PRINT(x) printf("\nNumber after setting the bit %d: %hu\n", n_bit, *p_num);  // %hhu, %hu, %u, %d, %ld, %lld
+#define PRINT(x) printf("\nNumber after setting the bit %d: %hu\n", n_bit, *p_num);  // %hhu, %hu, %u, %d, %ld, %lld, %lu, %llu
+
+
+bool valid_range_number(long long int num, unsigned char last_bit) {
+
+    return num >=0 || num < (long long int)pow(2, last_bit + 1);  // true if it's inside valid range
+}
+
+bool valid_range_position(long long int n_bit, unsigned char last_bit) {
+
+    return n_bit >= 0 || n_bit <= last_bit;  // true if it's inside valid range
+}
+
+
+long long int read_input(char last_bit, valid_range vr, char* text_str) {
+
+    long long int input_number;
+    
+    while(true) {
+
+        printf("%s", text_str);
+        scanf("%lld", &input_number);
+
+        if(vr(input_number, last_bit) == true)
+            break;
+        else
+            printf("Invalid number for the chosen type (negative or too high), try again.\n");
+    }
+
+    return input_number;
+}
 
 
 void print_binary(wildcard_type num, unsigned char last_bit) {
@@ -27,10 +61,12 @@ void print_binary(wildcard_type num, unsigned char last_bit) {
     return;
 }
 
+
 bool read_bit(wildcard_type num, char n_bit) {
 
     return num & (MASK << n_bit);
 }
+
 
 void set_bit(wildcard_type* p_num, char n_bit) {
 
@@ -40,46 +76,42 @@ void set_bit(wildcard_type* p_num, char n_bit) {
     return;
 }
 
+
 int main() {
 
     wildcard_type num;
 
     unsigned char n_bit, last_bit;  // you can use char, but left/right shift operator will promote to int either way... see documentation/drafts
-    last_bit = 8 * (unsigned char)sizeof(wildcard_type) - 1;
+
+    char last_bit_str[STR_TEXT_SIZE];
 
     bool is_set = false;
 
-    char* set_text_array[] = {"ALREADY SET, does not need to set..", "NOT SET"};
+
+    // define last_bit
+
+    last_bit = 8 * (unsigned char) sizeof(wildcard_type) - 1;
+    sprintf(last_bit_str, "\nEnter the bit position to set (0...%d): ", last_bit);
+
 
     // input number
 
-    while(true) {
-        long long int num_tmp;
-        printf("Enter an integer number: ");
-        scanf("%lld", &num_tmp);
-
-        if(num_tmp < (long long int)pow(2, last_bit + 1)) {
-            num = (wildcard_type)num_tmp;
-            break;
-        }
-        else
-            printf("Invalid number for the chosen type (too high), try again.\n");
-    }
+    num = (wildcard_type) read_input(last_bit, valid_range_number, "Enter an integer number: ");
 
     print_binary(num, last_bit);
 
+
     // input bit position
 
-    do {
-        printf("\nEnter the bit position to set (0...%d): ", last_bit);
-        scanf("%hhu", &n_bit);
-    } while(n_bit < 0 || n_bit > last_bit);
+    n_bit = (char) read_input(last_bit, valid_range_position, last_bit_str);
+
 
     // verifying if bit is set
 
     is_set = read_bit(num, n_bit);
 
-    printf("\nThe bit %d of this number is %s.\n", n_bit, is_set ? set_text_array[0] : set_text_array[1]);
+    printf("\nThe bit %d of this number is %s.\n", n_bit, is_set ? "ALREADY SET, does not need to set.." : "NOT SET");
+
 
     // setting the bit
 
